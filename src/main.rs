952a634369f0b612797b4ad099e3e3e517376f28
Clément Fournier <clement.fournier76@gmail.com>
2020-07-12 21:04:15 +0200

    Throw everything away

--- src/main.rs
+++ src/main.rs
@@ -12,106 +12,106 @@ use std::borrow::BorrowMut;
 mod reactors;
 
 fn main() {
-    let mut world =
-        Assembler::<WorldReactor>::root().assemble_subreactor::<AppReactor>();
-
-    let state_box= world.description;
-    let AppReactor { consumer, producer, .. } = state_box;
-
-    consumer.description.react_print.fire(&consumer.description, &mut consumer.state);
-    producer.description.react_incr.fire(&producer.description, &mut producer.state);
-    consumer.description.react_print.fire(&consumer.description, &mut consumer.state);
+    // let mut world =
+    //     Assembler::<WorldReactor>::root().assemble_subreactor::<AppReactor>();
+    //
+    // let state_box= world.description;
+    // let AppReactor { consumer, producer, .. } = state_box;
+    //
+    // consumer.description.react_print.fire(&consumer.description, &mut consumer.state);
+    // producer.description.react_incr.fire(&producer.description, &mut producer.state);
+    // consumer.description.react_print.fire(&consumer.description, &mut consumer.state);
 }
+//
+// // toplevel reactor containing the others todo hide as implementation detail
+// pub struct WorldReactor;
+//
+// impl Reactor for WorldReactor {
+//     type State = ();
+//
+//     fn new<'a>(_: &mut Assembler<'a, Self>) -> (Self, ()) where Self: 'a {
+//         (WorldReactor, ())
+//     }
+// }
 
-// toplevel reactor containing the others todo hide as implementation detail
-pub struct WorldReactor;
+//
+// // Links the other two reactors
+// pub struct AppReactor<'a> {
+//     producer: Linked<RunnableReactor<'a, ProduceReactor<'a>>>,
+//     consumer: Linked<RunnableReactor<'a, ConsumeReactor<'a>>>,
+//     _phantom_a: PhantomData<&'a ()>,
+// }
+//
+// impl<'b> Reactor for AppReactor<'b> {
+//     type State = ();
+//
+//     fn new<'a>(assembler: &mut Assembler<'a, Self>) -> (Self, ()) where 'b : 'a {
+//         let producer = assembler.assemble_subreactor::<ProduceReactor>();
+//         let consumer = assembler.assemble_subreactor::<ConsumeReactor>();
+//
+//         assembler.connect(&producer.description.out_value, &consumer.description.in_value);
+//
+//         (AppReactor { producer, consumer, _phantom_a: PhantomData }, ())
+//     }
+// }
 
-impl Reactor for WorldReactor {
-    type State = ();
-
-    fn new<'a>(_: &mut Assembler<'a, Self>) -> (Self, ()) where Self: 'a {
-        (WorldReactor, ())
-    }
-}
-
-
-// Links the other two reactors
-pub struct AppReactor<'a> {
-    producer: Linked<RunnableReactor<'a, ProduceReactor<'a>>>,
-    consumer: Linked<RunnableReactor<'a, ConsumeReactor<'a>>>,
-    _phantom_a: PhantomData<&'a ()>,
-}
-
-impl<'b> Reactor for AppReactor<'b> {
-    type State = ();
-
-    fn new<'a>(assembler: &mut Assembler<'a, Self>) -> (Self, ()) where 'b : 'a {
-        let producer = assembler.assemble_subreactor::<ProduceReactor>();
-        let consumer = assembler.assemble_subreactor::<ConsumeReactor>();
-
-        assembler.connect(&producer.description.out_value, &consumer.description.in_value);
-
-        (AppReactor { producer, consumer, _phantom_a: PhantomData }, ())
-    }
-}
-
-
-#[derive(Debug)]
-pub struct ProduceReactor<'a> {
-    /// This is the ouput port, that should be borrowed
-    // output: RefCell<i32>,
-    // output_borrower: Rc<RefCell<i32>>,
-
-    pub out_value: Linked<OutPort<i32>>,
-
-    pub react_incr: Linked<Reaction<'a, Self>>,
-    _phantom_a: PhantomData<&'a ()>,
-
-}
-
-impl<'b> Reactor for ProduceReactor<'b> {
-    type State = ();
-
-    fn new<'a>(assembler: &mut Assembler<'a, Self>) -> (Self, ()) where 'b : 'a {
-        let out_value = OutPort::new(assembler, "value", 0);
-        let react_incr = Reaction::new(
-            assembler,
-            "incr",
-            |r: &ProduceReactor, _| *r.out_value.get_mut() += 2,
-        );
-
-        link_reaction!((&react_incr) with (assembler)
-            (uses)
-            (affects &out_value)
-        );
-        (ProduceReactor { out_value, react_incr, _phantom_a: PhantomData }, ())
-    }
-}
-
-#[derive(Debug)]
-pub struct ConsumeReactor<'a> {
-    pub in_value: Linked<InPort<i32>>,
-    pub react_print: Linked<Reaction<'a, Self>>,
-    _phantom_a: PhantomData<&'a ()>,
-}
-
-impl<'b> Reactor for ConsumeReactor<'b> {
-    type State = ();
-
-    fn new<'a>(assembler: &mut Assembler<'a, Self>) -> (Self, ()) where 'b : 'a {
-        let in_value = InPort::new(assembler, "value");
-        let react_print = Reaction::new(
-            assembler,
-            "print_input",
-            |r: &ConsumeReactor, _| println!("Value is {}", port_value!(*r.in_value)),
-        );
-
-
-        link_reaction!((&react_print) with (assembler)
-            (uses &in_value)
-            (affects)
-        );
-
-        (ConsumeReactor { in_value, react_print, _phantom_a: PhantomData }, ())
-    }
-}
+//
+// #[derive(Debug)]
+// pub struct ProduceReactor<'a> {
+//     /// This is the ouput port, that should be borrowed
+//     // output: RefCell<i32>,
+//     // output_borrower: Rc<RefCell<i32>>,
+//
+//     pub out_value: Linked<OutPort<i32>>,
+//
+//     pub react_incr: Linked<Reaction<'a, Self>>,
+//     _phantom_a: PhantomData<&'a ()>,
+//
+// }
+//
+// impl<'b> Reactor for ProduceReactor<'b> {
+//     type State = ();
+//
+//     fn new<'a>(assembler: &mut Assembler<'a, Self>) -> (Self, ()) where 'b : 'a {
+//         let out_value = OutPort::new(assembler, "value", 0);
+//         let react_incr = Reaction::new(
+//             assembler,
+//             "incr",
+//             |r: &ProduceReactor, _| *r.out_value.get_mut() += 2,
+//         );
+//
+//         link_reaction!((&react_incr) with (assembler)
+//             (uses)
+//             (affects &out_value)
+//         );
+//         (ProduceReactor { out_value, react_incr, _phantom_a: PhantomData }, ())
+//     }
+// }
+//
+// #[derive(Debug)]
+// pub struct ConsumeReactor<'a> {
+//     pub in_value: Linked<InPort<i32>>,
+//     pub react_print: Linked<Reaction<'a, Self>>,
+//     _phantom_a: PhantomData<&'a ()>,
+// }
+//
+// impl<'b> Reactor for ConsumeReactor<'b> {
+//     type State = ();
+//
+//     fn new<'a>(assembler: &mut Assembler<'a, Self>) -> (Self, ()) where 'b : 'a {
+//         let in_value = InPort::new(assembler, "value");
+//         let react_print = Reaction::new(
+//             assembler,
+//             "print_input",
+//             |r: &ConsumeReactor, _| println!("Value is {}", port_value!(*r.in_value)),
+//         );
+//
+//
+//         link_reaction!((&react_print) with (assembler)
+//             (uses &in_value)
+//             (affects)
+//         );
+//
+//         (ConsumeReactor { in_value, react_print, _phantom_a: PhantomData }, ())
+//     }
+// }
--- src/reactors/mod.rs
+++ src/reactors/mod.rs
@@ -1,8 +1,1 @@
-pub mod action;
-#[macro_use]
-pub mod assembler;
-pub mod reaction;
-#[macro_use]
-pub mod port;
 pub mod reactor;
-
