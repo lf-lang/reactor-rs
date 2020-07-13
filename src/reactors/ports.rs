61f4fed77530e0951d1166b6f22520655b4935a1
Clément Fournier <clement.fournier76@gmail.com>
2020-07-14 01:19:58 +0200

    Try fixing ports

--- src/reactors/ports.rs
+++ src/reactors/ports.rs
@@ -1,4 +1,5 @@
 use std::any::{Any, TypeId};
+use std::borrow::Borrow;
 use std::cell::{Ref, RefCell};
 use std::collections::{HashMap, HashSet};
 use std::collections::hash_set::IntoIter;
@@ -8,7 +9,110 @@ use std::ops::DerefMut;
 use std::rc::Rc;
 
 use crate::reactors::id::{GlobalId, Identified};
-use crate::reactors::ports::PortBinding::{PortBound, Unbound};
+use crate::reactors::ports::BindStatus::{PortBound, Unbound};
+use crate::reactors::util::borrow_mut;
+use std::collections::hash_map::RandomState;
+
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum PortKind { Input, Output }
+
+
+// fixme thread safety
+pub struct PortId<T> {
+    kind: PortKind,
+    global_id: GlobalId,
+    _phantom_t: PhantomData<T>,
+
+    upstream_binding: Rc<RefCell<Binding<T>>>,
+}
+
+impl<T> PortId<T> {
+    fn kind(&self) -> PortKind {
+        self.kind
+    }
+
+    pub(in super) fn forward_to(&self, downstream: &PortId<T>) -> Result<(), String> {
+        // let binding_borrow: &RefCell<Binding<T>> = Rc::borrow(&downstream.upstream_binding);
+
+        let mut mut_downstream_cell = (&downstream.upstream_binding).borrow_mut();
+        let (downstream_status, ref downstream_class) = *mut_downstream_cell;
+
+        match downstream_status {
+            BindStatus::PortBound => Err(format!("Port {} is already bound to another port", downstream.global_id)),
+            BindStatus::DependencyBound => Err(format!("Port {} receives values from a reaction", downstream.global_id)),
+            BindStatus::Unbound => {
+                let mut self_cell = self.upstream_binding.borrow_mut();
+                let (_, my_class) = self_cell.deref_mut();
+
+                my_class.downstreams.borrow_mut().insert(HashableBinding::new(downstream));
+
+                let new_binding = (BindStatus::PortBound, Rc::clone(&my_class));
+
+                downstream_class.set_upstream(my_class);
+                *mut_downstream_cell.deref_mut() = new_binding;
+
+                Ok(())
+            }
+        }
+    }
+
+
+    pub(in super) fn new(kind: PortKind, global_id: GlobalId, default: T) -> Self {
+        PortId::<T> {
+            kind,
+            global_id,
+            _phantom_t: PhantomData,
+            upstream_binding: Rc::new(RefCell::new((Unbound, Rc::new(PortEquivClass::<T>::new(default))))),
+        }
+    }
+}
+
+
+impl<T> Identified for PortId<T> {
+    fn global_id(&self) -> &GlobalId {
+        &self.global_id
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash)]
+enum BindStatus {
+    Unbound,
+    PortBound,
+    DependencyBound,
+}
+
+
+type Binding<T> = (BindStatus, Rc<PortEquivClass<T>>);
+
+struct HashableBinding<T> {
+    cell: Rc<RefCell<Binding<T>>>,
+    key: GlobalId,
+}
+
+impl<T> HashableBinding<T> {
+    fn new(port: &PortId<T>) -> HashableBinding<T> {
+        HashableBinding {
+            key: port.global_id.clone(),
+            cell: Rc::clone(&port.upstream_binding),
+        }
+    }
+}
+
+impl<T> PartialEq for HashableBinding<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.key == other.key
+    }
+}
+
+impl<T> Eq for HashableBinding<T> {}
+
+impl<T> Hash for HashableBinding<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.key.hash(state)
+    }
+}
+
 
 /// An equivalence class is a set of ports that are
 /// bound together transitively. Then, if anyone is
@@ -21,91 +125,25 @@ use crate::reactors::ports::PortBinding::{PortBound, Unbound};
 struct PortEquivClass<T> {
     // This the container for the value
     cell: RefCell<T>,
+
+    downstreams: RefCell<HashSet<HashableBinding<T>>>,
 }
 
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum PortKind { Input, Output }
-
-// fixme thread safety
-pub struct PortId<T> {
-    kind: PortKind,
-    global_id: GlobalId,
-    _phantom_t: PhantomData<T>,
-
-    /// Ports have a slot in which they accumulate values.
-    /// The outer RefCell lets us change the binding internally.
-    /// The inner one stores values.
-    ///
-    /// TODO you may be able to get rid of the outer RefCells by using mut references
-    upstream_binding: RefCell<(PortBinding, Rc<PortEquivClass<T>>)>,
-
-    downstream: HashSet<Rc<PortId<T>>>,
-}
-
-impl<T> PortId<T> {
-    fn kind(&self) -> PortKind {
-        self.kind
-    }
-
-    pub(in super) fn forward_to(&mut self, downstream: Rc<PortId<T>>) -> Result<(), String> {
-        let mut downstream_cell = downstream.upstream_binding.borrow_mut(); // reserve the binding
-
-        match *downstream_cell {
-            (PortBinding::PortBound, _) => Err(format!("Port {} is already bound to another port", downstream.global_id)),
-            (PortBinding::DependencyBound, _) => Err(format!("Port {} receives values from a reaction", downstream.global_id)),
-            (PortBinding::Unbound, _) => {
-                let (_, my_class) = &*self.upstream_binding.borrow();
-
-                let new_binding = (PortBinding::PortBound, Rc::clone(&my_class));
-                let my_downstream = &mut self.downstream;
-                for x in &downstream.downstream {
-                    *x.upstream_binding.borrow_mut() = new_binding.clone();
-                    my_downstream.insert(x.clone());
-                }
-                *downstream_cell.deref_mut() = new_binding;
-                my_downstream.insert(Rc::clone(&downstream));
-                Ok(())
-            }
+impl<T> PortEquivClass<T> {
+    fn new(initial: T) -> Self {
+        PortEquivClass {
+            cell: RefCell::new(initial),
+            downstreams: Default::default(),
         }
     }
 
-
-    pub(in super) fn new(kind: PortKind, global_id: GlobalId, default: T) -> Self {
-        PortId::<T> {
-            kind,
-            global_id,
-            _phantom_t: PhantomData,
-            upstream_binding: RefCell::new((Unbound, Rc::new(PortEquivClass { cell: RefCell::<T>::new(default) }))),
-            downstream: Default::default(),
+    /// This updates all downstreams to point to the given equiv class instead of `self`
+    fn set_upstream(&self, new_binding: &Rc<PortEquivClass<T>>) {
+        for hashed in &*self.downstreams.borrow() {
+            let cell: &RefCell<Binding<T>> = Rc::borrow(&hashed.cell);
+            let mut ref_mut = cell.borrow_mut();
+            let b: Binding<T> = (ref_mut.0, Rc::clone(new_binding));
+            *ref_mut.deref_mut() = b;
         }
     }
-}
-
-impl<T> PartialEq<Self> for PortId<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.global_id == other.global_id
-    }
-}
-
-impl<T> Eq for PortId<T> {}
-
-impl<T> Hash for PortId<T> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.global_id.hash(state)
-    }
-}
-
-impl<T> Identified for PortId<T> {
-    fn global_id(&self) -> &GlobalId {
-        &self.global_id
-    }
-}
-
-
-#[derive(Clone)]
-enum PortBinding {
-    Unbound,
-    PortBound,
-    DependencyBound,
 }
--- src/reactors/util.rs
+++ src/reactors/util.rs
@@ -1,3 +1,7 @@
+use std::cell::{RefCell, RefMut};
+use std::rc::Rc;
+use std::borrow::Borrow;
+use std::ops::DerefMut;
 
 /// A type whose instances have statically known names
 pub trait Named {
@@ -7,4 +11,9 @@ pub trait Named {
 /// A type that can list all its instances
 pub trait Enumerated {
     fn list() -> Vec<Self> where Self: Sized;
+}
+
+
+pub fn borrow_mut<T>(cell: &Rc<RefCell<T>>) -> RefMut<T> {
+    cell.borrow_mut()
 }
