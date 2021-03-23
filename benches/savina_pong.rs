ac5158ecb6cf4bd673226ff0a57e5547a9be8199
Clément Fournier <clement.fournier76@gmail.com>
2021-03-23 18:31:12 +0100

    Remove some numbers from the benchmark
    It's useless to test below 1000 iterations, as we just
    end up with noise

--- benches/savina_pong.rs
+++ benches/savina_pong.rs
@@ -27,7 +27,7 @@ criterion_main!(benches);
 
 fn reactor_main(c: &mut Criterion) {
     let mut group = c.benchmark_group("savina_pong");
-    for num_pongs in [10, 100, 1000, 10_000, 50_000].iter() {
+    for num_pongs in [1000, 10_000, 50_000, 1_000_000].iter() {
         group.bench_with_input(
             BenchmarkId::from_parameter(num_pongs),
             num_pongs,
