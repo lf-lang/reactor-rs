82fd7ae7f6796e6f0e0a8f98ac78909e337227f8
Johannes Hayeß <jhaye@mailbox.org>
2022-01-18 10:48:44 +0100

    Merge fold and subsequent reduce of iterator into singular reduce

--- src/scheduler/scheduler_impl.rs
+++ src/scheduler/scheduler_impl.rs
@@ -507,8 +507,7 @@ mod parallel_rt_impl {
 
                 CloneableCtx(ctx)
             })
-            .fold(|| RContextForwardableStuff::default(), |cx1, cx2| cx1.merge(cx2.0.insides))
-            .reduce(|| Default::default(), RContextForwardableStuff::merge);
+            .reduce(|| RContextForwardableStuff::default(), |cx1, cx2| cx1.merge(cx2.0.insides));
     }
 
     #[derive(Copy, Clone)]
