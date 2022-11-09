707c77de8fef34562d75b72b8533899dd0f2ec15
oowekyala <oowekyala@users.noreply.github.com>
2022-11-09 15:25:00 +0000

    deploy: f0de4b917abcb6839e2a3dc4062dc896095b7957

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["reactor_rt"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.64.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"reactor_rt/enum.TimeUnit.html\" title=\"enum reactor_rt::TimeUnit\">TimeUnit</a>","synthetic":false,"types":["reactor_rt::util::TimeUnit"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"reactor_rt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"reactor_rt/enum.TimeUnit.html\" title=\"enum reactor_rt::TimeUnit\">TimeUnit</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
