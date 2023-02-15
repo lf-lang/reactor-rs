7dab5bb0ecbafac00a82b5b322e6a38e45cfb88b
jhaye <jhaye@users.noreply.github.com>
2023-02-15 14:40:09 +0000

    deploy: 73d6f6ce8db61d69cce6ac9d50666ac283ecbc82

--- implementors/core/ops/arith/trait.Add.js
+++ implementors/core/ops/arith/trait.Add.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reactor_rt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"reactor_rt/struct.MicroStep.html\" title=\"struct reactor_rt::MicroStep\">MicroStep</a>"]]
+"reactor_rt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.1/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"reactor_rt/struct.MicroStep.html\" title=\"struct reactor_rt::MicroStep\">MicroStep</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
