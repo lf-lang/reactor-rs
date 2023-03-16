b64ac545379cac2244e998db49c7dd97d8d60393
oowekyala <oowekyala@users.noreply.github.com>
2023-03-16 14:03:39 +0000

    deploy: 9c3b34d05587cd1288e50088ee394129bb58dcf8

--- implementors/core/convert/trait.From.js
+++ implementors/core/convert/trait.From.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"vecmap":[["impl&lt;K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;K&gt; for <a class=\"struct\" href=\"vecmap/struct.KeyRef.html\" title=\"struct vecmap::KeyRef\">KeyRef</a>&lt;K&gt;"]]
+"vecmap":[["impl&lt;K&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;K&gt; for <a class=\"struct\" href=\"vecmap/struct.KeyRef.html\" title=\"struct vecmap::KeyRef\">KeyRef</a>&lt;K&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
