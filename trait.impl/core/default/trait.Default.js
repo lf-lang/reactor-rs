acdd66b2202243e84d875fe9282244421a1282a0
oowekyala <oowekyala@users.noreply.github.com>
2024-05-14 14:21:45 +0000

    deploy: 584203464ee0eb7270e033da422122a7e83096e4

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reactor_rt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"reactor_rt/struct.SchedulerOptions.html\" title=\"struct reactor_rt::SchedulerOptions\">SchedulerOptions</a>"]]
+"reactor_rt":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"reactor_rt/struct.SchedulerOptions.html\" title=\"struct reactor_rt::SchedulerOptions\">SchedulerOptions</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
