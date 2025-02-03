0723e3ad4ccc156ade0165fa552fe475ef93494c
lhstrh <lhstrh@users.noreply.github.com>
2025-02-03 19:42:21 +0000

    deploy: ade0d7a6a558edc27fa6584ebe349ea30f8f3f3e

--- trait.impl/core/convert/trait.TryFrom.js
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["reactor_rt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"reactor_rt/enum.TimeUnit.html\" title=\"enum reactor_rt::TimeUnit\">TimeUnit</a>"]]]]);
+    var implementors = Object.fromEntries([["reactor_rt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"reactor_rt/enum.TimeUnit.html\" title=\"enum reactor_rt::TimeUnit\">TimeUnit</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/index/trait.IndexMut.js
+++ trait.impl/core/ops/index/trait.IndexMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["reactor_rt",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/index/trait.IndexMut.html\" title=\"trait core::ops::index::IndexMut\">IndexMut</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.0/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"reactor_rt/struct.Multiport.html\" title=\"struct reactor_rt::Multiport\">Multiport</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["reactor_rt",[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/index/trait.IndexMut.html\" title=\"trait core::ops::index::IndexMut\">IndexMut</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"reactor_rt/struct.Multiport.html\" title=\"struct reactor_rt::Multiport\">Multiport</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
