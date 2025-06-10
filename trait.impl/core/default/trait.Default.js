687a9ca6c091463327e3bfbfc5587a19be04e032
edwardalee <edwardalee@users.noreply.github.com>
2025-06-10 12:24:14 +0000

    deploy: 7b27416c19336a9cb649238c706cadc7aad728bf

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["reactor_rt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"reactor_rt/struct.SchedulerOptions.html\" title=\"struct reactor_rt::SchedulerOptions\">SchedulerOptions</a>"]]]]);
+    var implementors = Object.fromEntries([["reactor_rt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"reactor_rt/struct.SchedulerOptions.html\" title=\"struct reactor_rt::SchedulerOptions\">SchedulerOptions</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/ops/arith/trait.Add.js
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["reactor_rt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"reactor_rt/struct.MicroStep.html\" title=\"struct reactor_rt::MicroStep\">MicroStep</a>"]]]]);
+    var implementors = Object.fromEntries([["reactor_rt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"reactor_rt/struct.MicroStep.html\" title=\"struct reactor_rt::MicroStep\">MicroStep</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
